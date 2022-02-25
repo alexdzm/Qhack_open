74f1d3e58a3ba38eb200afc5697201b76e14ed80
Alex <63930909+alexdzm@users.noreply.github.com>
2022-02-25 17:50:02 +0000

    Submission commit

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Qhack_open
 
 
-This repository will contain the code for The Quantocks submission to QHack '22.
+This repository contains the code for The Quantocks submission to QHack '22. The code is incomplete and needs debugging as of 25/02/22
 
-Start with the notebooks in the root folder to see how the algorithm works.
+Make_HB contains the linear algebra needed to construct the hamiltoninan.
 
-The aim  of this project i sto implement the algorithm described in [this](https://arxiv.org/pdf/1812.10495.pdf) paper. The inital aim wil be to replicate results for the zero tempreture regime. After this, the finite tempreture algorithm will be considered. Finally, time permitting, we will look for data about other molecules to input to the algorithm.
+Zero_temp_examples is a notebook that starts to constuct the required circuit.
 
--- make_HB.py
+++ make_HB.py
@@ -4,7 +4,7 @@ from qiskit import QuantumCircuit
 from qiskit.extensions import UnitaryGate
 from qiskit.quantum_info.operators import Operator
 
-def a_dag(Lmax,i,verbose=False):
+def a_dag(Lmax,verbose=False):
     """
     TO DO: implement equation 7 and use a binary qubit mapping
     will be the backbone of the algorithm
@@ -17,8 +17,6 @@ def a_dag(Lmax,i,verbose=False):
     sig_10=np.array([[0,0],[1,0]])#sigma plus
     sig_00=np.array([[1,0],[0,0]])
     sig_11=np.array([[0,0],[0,1]])
-    
-    
     for l in range(1,Lmax): #NB start at 1 to prevent zero error
         l_b_string=format(l,'b') #converts the l value to a binary string
         l_b_string=l_b_string.zfill(num_qubits) #pads out this number with enough zeros to fill in values for each qubit
@@ -30,34 +28,36 @@ def a_dag(Lmax,i,verbose=False):
             if index==0:
                 
                 if digit=='0' and l_minus_b_string[index]=='0':
-                    a_dag=sig_00
+                    a_dag1=sig_00
                 elif digit=='1' and l_minus_b_string[index]=='1':
-                    a_dag=sig_11
+                    a_dag1=sig_11
                 elif digit=='0' and l_minus_b_string[index]=='1':
-                    a_dag=sig_01
+                    a_dag1=sig_01
                 elif digit=='1' and l_minus_b_string[index]=='1':
-                    a_dag=sig_10
+                    a_dag1=sig_10
             else:
                 if digit=='0' and l_minus_b_string[index]=='0':
-                    a_dag=np.tensordot(a_dag,sig_00,axes=0)
+                    a_dag1=np.tensordot(a_dag1,sig_00,axes=0)
                 elif digit=='1' and l_minus_b_string[index]=='1':
-                    a_dag=np.tensordot(a_dag,sig_11,axes=0)
+                    a_dag1=np.tensordot(a_dag1,sig_11,axes=0)
                 elif digit=='0' and l_minus_b_string[index]=='1':
-                    a_dag=np.tensordot(a_dag,sig_01,axes=0)
+                    a_dag1=np.tensordot(a_dag1,sig_01,axes=0)
                 elif digit=='1' and l_minus_b_string[index]=='1':
-                    a_dag=np.tensordot(a_dag,sig_10,axes=0)
+                    a_dag1=np.tensordot(a_dag1,sig_10,axes=0)
                     
             
         if l==1:
-            a_dag_tot=a_dag
+            a_dag_tot=a_dag1
         else:
-            a_dag_tot+=a_dag
+            a_dag_tot+=a_dag1
 
-    if verbose==True:
-            print(a_dag_tot)
+        print(l)
+    
+    print(a_dag_tot)
+    
     return a_dag_tot
 
-def b_dag(S,OhmA,OhmB,delta,Lmax,i):
+def b_dag(S,omegaB,OhmA,OhmB,delta,Lmax):
     """
     COMPLETE (hopefully)
     Takes the Ohm matricies of the two PES and uses them to transform the inital
@@ -74,10 +74,13 @@ def b_dag(S,OhmA,OhmB,delta,Lmax,i):
     Returns:
         b (vector): excited state creation operator
     """
-    a=a_dag(Lmax,i).conj().T
+    a_dag1=a_dag(Lmax)
+    a_vec=[a_dag1.conj().T,a_dag1.conj().T] #nb this will limit us to 2D omega vectors 
+    a_dag_vec=[a_dag1,a_dag1]
+
     J=np.matmul(OhmB,np.matmul(S,np.linalg.inv(OhmA)))
-    ans=0.5*np.matmul((J-np.linalg.inv(np.transpose(J))),a)
-    + 0.5*np.matmul((J+np.linalg.inv(np.transpose(J))),a.conj().T)
+    ans=0.5*np.matmul((J-np.linalg.inv(np.transpose(J))),a_vec)
+    + 0.5*np.matmul((J+np.linalg.inv(np.transpose(J))),a_dag_vec)
     +(1/math.sqrt(2))*np.array(delta)
     return ans
 
@@ -93,11 +96,18 @@ def HB(S,omegaB,OhmA,OhmB,delta,Lmax):
     Returns:
         _type_: _description_
     """    
+    b_dag_vec=b_dag(S,omegaB,OhmA,OhmB,delta,Lmax)
+    b_vec=b_dag_vec
+    for i in range(len(b_vec)):
+        b_vec[i]=b_dag_vec[i].conj().T
 
     for i in range(len(omegaB)):
-        b_dag_i=b_dag(S,OhmA,OhmB,delta,Lmax,i)
         if i==0:
-            HB=omegaB[i]*(np.matmul(b_dag_i,b_dag_i.conj().T+0.5))
+            HB=omegaB[i]*(np.matmul(b_dag_vec[i],b_vec[i])+0.5)
         else:
-            HB+=omegaB[i]*(np.matmul(b_dag_i,b_dag_i.conj().T+0.5))
+            HB+=omegaB[i]*(np.matmul(b_dag_vec[i],b_vec[i])+0.5)
+    b_vec=[]
+    for i in range(len(omegaB)):
+        b_vec.append(b_dag(S,OhmA,OhmB,delta,Lmax,i))
+
     return HB
